ed922365dce9a95e817899614c64ae870e323313
JKomieter <komieterj@gmail.com>
2023-08-12 16:33:05 -0400

    fixed the Error: can be escaped with &apos;, &lsquo;, &#39;, &rsquo; errror for first deployment

--- jkportfolio/components/Skills/SkillsInfo.tsx
+++ jkportfolio/components/Skills/SkillsInfo.tsx
@@ -4,7 +4,7 @@ import SkillItem from "./SkillItem";
 
 const rubik = Rubik({
     subsets: ["latin"],
-    weights: ["400", "500", "600", "700"],
+    weight: ["400", "500", "600", "700"],
 });
 
 const SkillsInfo = () => {
--- jkportfolio/components/layout/Heros/RightHero.tsx
+++ jkportfolio/components/layout/Heros/RightHero.tsx
@@ -1,12 +1,11 @@
 import { HeroSvg } from "@/components/svg";
 
-
 const RightHero = () => {
-    return (
-      <div className="w-full">
-        <HeroSvg className="lg:text-8xl md:text-4xl sm:text-3xl text-2xl font-semibold w-full h-full" />
-      </div>
-    );
-}
+  return (
+    <div className="w-full">
+      <HeroSvg className="lg:text-8xl md:text-4xl sm:text-3xl text-2xl font-semibold w-full h-full" />
+    </div>
+  );
+};
 
 export default RightHero;
